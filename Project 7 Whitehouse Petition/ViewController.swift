149420db7c26ecb56e5081d2c873ef515bee667b
A W <waq.mmhy@live.ca>
2019-07-02 21:34:02 -0400

    Initial Commit

---
+++ Project 7 Whitehouse Petition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 7 Whitehouse Petition
+//
+//  Created by Angela Wu  on 2019-07-02.
+//  Copyright © 2019 Angela Wu . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
